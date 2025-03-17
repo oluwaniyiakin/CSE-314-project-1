5f5d72c5ccdb473ea2fbf27fa72714465208a242
OLUWANIYIAKIN <olu24002@byui.edu>
2025-03-17 16:46:09 +0100

    ss

--- server.js
+++ server.js
@@ -22,4 +22,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use("/contacts", contactsRouter);
 app.use("/companies", companiesRouter);
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the CSE 340 API! Use /contacts or /companies.");
+  });
+  
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
